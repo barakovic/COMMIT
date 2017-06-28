167477ea0ebb3d8505051e019fa90f37407dd7ed
Muhamed Baraković <muhamed.barakovic@epfl.ch>
2017-06-28 09:23:51 +0100

    Add diffusivities

--- commit/models.py
+++ commit/models.py
@@ -14,8 +14,16 @@ class CylinderZeppelinBall( amico.models.CylinderZeppelinBall ) :
     """
     pass
 
+
 class VolumeFractions( amico.models.VolumeFractions ) :
     """Simulate the response functions according to the VolumeFractions model.
+    See the AMICO.model module for details.
+    """
+    pass
+
+
+class StickZeppelinBallMultiDiffusivity( amico.models.StickZeppelinBallMultiDiffusivity ) :
+    """Simulate the response functions according to the Stick-Zeppelin-Ball model with different diffusivities.
     See the AMICO.model module for details.
     """
     pass
